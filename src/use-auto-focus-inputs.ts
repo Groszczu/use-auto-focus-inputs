13df5e3c813e826e0cc2d2e489a0c07de9a6acb0
Roch Goszczyński <roch.goszczynski@gmail.com>
2021-08-05 17:34:29 +0200

    Change use-auto-focus-inputs typing

--- src/use-auto-focus-inputs.ts
+++ src/use-auto-focus-inputs.ts
@@ -8,65 +8,60 @@ import {
 import callAll from './call-all';
 import mergeRefs from './merge-refs';
 
-type TextInputRef = React.RefObject<TextInput> | React.Ref<TextInput | null>;
+type UseAutoFocusInputsReturnType = <
+  TProps extends TextInputProps & {ref?: React.Ref<TextInput>},
+>(
+  props: TProps,
+) => unknown;
 
-function useAutoFocusInputs({initialRerender = true} = {}): <TProps>(
-  props: TextInputProps & {ref?: TextInputRef} & TProps,
-) => TextInputProps & {ref: React.Ref<TextInput>} & TProps {
+function useAutoFocusInputs({
+  initialRerender = true,
+} = {}): UseAutoFocusInputsReturnType {
   const inputRefsRef = React.useRef<React.RefObject<TextInput>[]>([]);
   const inputIndexRef = React.useRef(-1);
-  const [rendered, setRendered] = React.useState(false);
+  const [rerendered, setRerendered] = React.useState(false);
 
   React.useEffect(() => {
-    // reset input index or rerender
+    // reset input index on rerender
     inputIndexRef.current = -1;
   });
 
   React.useEffect(() => {
-    if (!rendered && initialRerender) {
-      setRendered(true);
+    if (!rerendered && initialRerender) {
+      setRerendered(true);
     }
-  }, [initialRerender, rendered]);
+  }, [initialRerender, rerendered]);
 
-  return React.useCallback(
-    <TProps>({
-      ref,
-      onSubmitEditing,
-      ...props
-    }: TextInputProps & {
-      ref?: TextInputRef;
-    } & TProps): TextInputProps & {ref: React.Ref<TextInput>} & TProps => {
-      inputIndexRef.current += 1;
-      const currentInputIndex = inputIndexRef.current;
+  return React.useCallback(({ref, onSubmitEditing, ...props}) => {
+    inputIndexRef.current += 1;
+    const currentInputIndex = inputIndexRef.current;
 
-      let inputRef = inputRefsRef.current[currentInputIndex];
-      if (!inputRef) {
-        inputRef = React.createRef<TextInput>();
-        inputRefsRef.current.push(inputRef);
-      }
+    let inputRef = inputRefsRef.current[currentInputIndex];
+    if (!inputRef) {
+      inputRef = React.createRef<TextInput>();
+      inputRefsRef.current.push(inputRef);
+    }
 
-      const nextInput = inputRefsRef.current[currentInputIndex + 1];
-      return {
-        ref: mergeRefs(ref, inputRef),
-        returnKeyType: nextInput ? 'next' : 'done',
-        blurOnSubmit: !nextInput,
-        onSubmitEditing: callAll(
-          nextInput
-            ? // eslint-disable-next-line @typescript-eslint/no-unused-vars
-              (_e: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
-                const nextInputRef = nextInput?.current;
-                if (nextInputRef) {
-                  nextInputRef.focus();
-                }
+    const nextInput = inputRefsRef.current[currentInputIndex + 1];
+    return {
+      ref: mergeRefs(ref, inputRef),
+      returnKeyType: nextInput ? 'next' : 'done',
+      blurOnSubmit: !nextInput,
+      onSubmitEditing: callAll(
+        nextInput
+          ? // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            (_e: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
+              const nextInputRef = nextInput?.current;
+              if (nextInputRef) {
+                nextInputRef.focus();
               }
-            : undefined,
-          onSubmitEditing,
-        ),
-        ...props,
-      } as TextInputProps & {ref: React.Ref<TextInput>} & TProps;
-    },
-    [],
-  );
+            }
+          : undefined,
+        onSubmitEditing,
+      ),
+      ...props,
+    };
+  }, []);
 }
 
 export default useAutoFocusInputs;
